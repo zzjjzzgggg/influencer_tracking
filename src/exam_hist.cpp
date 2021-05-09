9d9a12540869b4bed863b4de5c057b28314c5650
3650209 <1627982051@qq.com>
2021-05-09 22:04:07 +0800

    test for HISTPDG

--- src/exam_hist.cpp
+++ src/exam_hist.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "hist_pdg.h"
+#include "basic_pdg.h"
 #include "ISet_segment.h"
 #include "lifespan_generator.h"
+#include "dyn_dgraph_mgr_v2.h"
 //#include "dyn_bgraph_mgr.h"
 int main(){
-    int L=10;
+    int L=40;
     int budget=10;
     double eps=0.2;
     int num_samples=10;
@@ -21,6 +23,7 @@ int main(){
     int x=0;
     SocialAcs ss;
     SocialAcs sc;
+
     while(getline(data,oneline)){
         std::istringstream read_str(oneline);
         int item;
@@ -30,17 +33,22 @@ int main(){
         SocialAc soca=std::make_pair(std::make_pair(temp[0],temp[1]),temp[2]);
         sc.push_back(soca);
         x++;
-        if(x==200)
+        if(x==1000)
             break;
     }
 
     HistPDG hist(num_samples,budget,eps);
+    BasicPAG<DynDGraphMgr> basic(L,budget,eps,num_samples);
     ss.push_back({{1,2},1619009223});
     ss.push_back({{1,2},1619009223});
     ss.push_back({{1,3},1619009223});
     ss.push_back({{3,4},1619009223});
     ss.push_back({{4,5},1619009223});
+    int i=1;
 
+    int temp=1;
+    std::string outfile="hist.txt";
+    std::ofstream out(outfile);
     for(auto &s:sc){
         std::vector<int> lifespan=lifegen.getLifespans(num_samples);
         ISetSegments segs(lifespan);
@@ -48,10 +56,21 @@ int main(){
 //            std::cout<<"start"<<seg.start_<<" "<<seg.end_<<std::endl;
 //        }
         hist.feed(s,segs);
+        std::cout<<i++<<" ";
+        double hist_mx=hist.getResult();
+        std::cout<<"hist"<<hist_mx<<"   ";
+//        hist.reduce();
 
-        double pag_mx=hist.getResult();
-        std::cout<<pag_mx<<std::endl;
         hist.next();
+
+        basic.update(s,segs);
+
+        double basic_mx=basic.getResult();
+        std::cout<<"basic"<<basic_mx<<std::endl;
+        basic.next();
+
+        out<<temp<<'\t'<<basic_mx<<'\t'<<hist_mx<<std::endl;
+        temp++;
     }
 
     return 0;
