08ec70d40d50c10ff31cc3ea7dc16e18db7e9831
J. Zhao <zzjjzzgggg@gmail.com>
2021-06-09 21:24:56 +0800

    code clean

--- src/histit_red.h
+++ src/histit_red.h
@@ -8,27 +8,30 @@
 #include "iset_segment.h"
 #include "sieve_pait.h"
 
-class HistITRED{
+class HistITRED {
 private:
-    class Alg{
+    class Alg {
     private:
         SievePAIT* sieve_ptr_;
+
     public:
-        int l_;//instance index
-        double val_=0;//current utility value
-        double delta_=0;//uncertainty of this instance
-        double uncertainty_=0;//uncertainty of this interval
+        int l_;                   // instance index
+        double val_ = 0;          // current utility value
+        double delta_ = 0;        // uncertainty of this instance
+        double uncertainty_ = 0;  // uncertainty of this interval
     public:
-        Alg(const int l,const int num_samples,const int budget,const double eps):l_(l){
-            sieve_ptr_=new SievePAIT(num_samples,budget,eps);
+        Alg(const int l, const int num_samples, const int budget,
+            const double eps)
+            : l_(l) {
+            sieve_ptr_ = new SievePAIT(num_samples, budget, eps);
         }
         Alg(const Alg& o) : l_(o.l_), val_(o.val_) {
             sieve_ptr_ = new SievePAIT(*o.sieve_ptr_);
         }
 
-        virtual ~Alg(){delete sieve_ptr_;}
+        virtual ~Alg() { delete sieve_ptr_; }
 
-        inline void feed(const UVC &a, const ISet& is) {
+        inline void feed(const UVC& a, const ISet& is) {
             sieve_ptr_->update(a, is);
             val_ = sieve_ptr_->getResult();
         }
@@ -36,7 +39,7 @@ private:
         inline double upper() const { return val_ + delta_; }
         inline double lower() const { return val_; }
 
-        inline void clear(){sieve_ptr_->clear();}
+        inline void clear() { sieve_ptr_->clear(); }
     };
 
 private:
@@ -45,72 +48,69 @@ private:
     double eps_;
 
     std::list<Alg*> algs_;
+
 public:
     HistITRED(const int num_samples, const int budget, const double eps)
-            : num_samples_(num_samples), budget_(budget), eps_(eps) {
-    }
+        : num_samples_(num_samples), budget_(budget), eps_(eps) {}
 
-    virtual ~ HistITRED() {
+    virtual ~HistITRED() {
         for (auto it = algs_.begin(); it != algs_.end(); ++it) delete *it;
     }
 
     void newEndIfNeed(const int l);
 
-    void feed(const UVC &a,const ISetSegments& segs);
+    void feed(const UVC& a, const ISetSegments& segs);
 
-    void feedSegment(const UVC &a, const ISetSegment& seg,
+    void feedSegment(const UVC& a, const ISetSegment& seg,
                      typename std::list<Alg*>::iterator& it);
 
     double getResult() const { return algs_.front()->val_; }
     void next();
     void reduce();
-    void process(const SocialAc a, const int l, const int r, const ISetSegment& seg);
+    void process(const SocialAc a, const int l, const int r,
+                 const ISetSegment& seg);
 };
 void HistITRED::newEndIfNeed(const int l) {
     if (algs_.empty() || algs_.back()->l_ < l)
         algs_.push_back(new Alg(l, num_samples_, budget_, eps_));
 }
 
-void HistITRED::feed(const UVC &a, const ISetSegments& segs){
-
-    auto it=algs_.begin();
-    for(auto& seg:segs.segments_){
-        //create a new head instance if necessary
-        if(algs_.empty()) {
+void HistITRED::feed(const UVC& a, const ISetSegments& segs) {
+    auto it = algs_.begin();
+    for (auto& seg : segs.segments_) {
+        // create a new head instance if necessary
+        if (algs_.empty()) {
             algs_.push_back(new Alg(seg.end_ - 1, num_samples_, budget_, eps_));
             it = algs_.begin();
         }
-        if (algs_.back()->l_ < seg.end_-1){
-            algs_.push_back(new Alg(seg.end_-1, num_samples_, budget_, eps_));
-
+        if (algs_.back()->l_ < seg.end_ - 1) {
+            algs_.push_back(new Alg(seg.end_ - 1, num_samples_, budget_, eps_));
         }
-        //Update instances belonging to this segment.
-        feedSegment(a,seg,it);
+        // Update instances belonging to this segment.
+        feedSegment(a, seg, it);
 
-        auto pre=it;
-        //if last updated alg.l_=l,continue
+        auto pre = it;
+        // if last updated alg.l_=l,continue
         if (it != algs_.begin()) {
             --pre;  // let "pre" be the precessor of "it"
             // no need to create a new instance
             if ((*pre)->l_ == seg.end_ - 1) continue;
         }
 
-        //create new alg from successor
-        if(it==algs_.begin()||(*pre)->l_<(seg.end_-1)){
-            //create alg_l based on its successor
-            Alg* alg=new Alg(*(*it));
-            alg->l_=seg.end_-1;
+        // create new alg from successor
+        if (it == algs_.begin() || (*pre)->l_ < (seg.end_ - 1)) {
+            // create alg_l based on its successor
+            Alg* alg = new Alg(*(*it));
+            alg->l_ = seg.end_ - 1;
             alg->delta_ = alg->uncertainty_ = (*it)->uncertainty_;
             alg->clear();
-            alg->feed(a,seg.is_);
-            algs_.insert(it,alg);
+            alg->feed(a, seg.is_);
+            algs_.insert(it, alg);
         }
     }
-
-
 }
 // Update instances belonging to this segment.
-void HistITRED::feedSegment(const UVC &a, const ISetSegment& seg,
+void HistITRED::feedSegment(const UVC& a, const ISetSegment& seg,
                             std::list<Alg*>::iterator& it) {
     while (it != algs_.end() && (*it)->l_ < seg.end_) {
         (*it)->feed(a, seg.is_);
@@ -132,12 +132,12 @@ void HistITRED::next() {
 }
 
 void HistITRED::reduce() {
-    auto i=algs_.begin();
-    //for each i,find the largest j such that A(j) >= (1-eps)(A(i)+beta(j)).
+    auto i = algs_.begin();
+    // for each i,find the largest j such that A(j) >= (1-eps)(A(i)+beta(j)).
     while (i != algs_.end()) {
         auto j = i, l = i;
         if (++j == algs_.end()) break;
-        //calculate the bound
+        // calculate the bound
         double bound = (*i)->upper() * (1 - eps_);
         while (j != algs_.end() && (*j)->lower() >= bound) ++j;
         if (--j == i || ++l == j) {
@@ -152,12 +152,11 @@ void HistITRED::reduce() {
             delete *l;
             ++l;
         }
-        //get the uncertainty of this interval
+        // get the uncertainty of this interval
         (*j)->uncertainty_ = (*i)->upper() * eps_;
         algs_.erase(++i, j);
         i = j;
     }
 }
 
-
-#endif //INFLUENCERS_TRACKING_HISTIT_RED_H
+#endif  // INFLUENCERS_TRACKING_HISTIT_RED_H
