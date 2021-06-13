4c773be7e9162efd52c258edf04b99d6426c4cd6
3650209 <1627982051@qq.com>
2021-06-13 10:34:01 +0800

    sample num experiment

--- src/exam_samples.cpp
+++ src/exam_samples.cpp
@@ -5,17 +5,17 @@
 #include "social_influence.h"
 #include "iset_segment.h"
 #include <gflags/gflags.h>
-#include "samples.h"
 
 DEFINE_string(dir, "", "working directory");
-DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_int32(n, 10, "number of samples");
+DEFINE_string(stream, "test.txt", "input streaming data file name");
+DEFINE_int32(n, 2000, "number of samples");
 DEFINE_double(lmd, .01, "decaying rate");
 DEFINE_int32(k,10,"number of picked users");
-DEFINE_int32(R, 20, "repeat");
 
-int main(){
+int main(int argc, char* argv[]){
     rngutils::default_rng rng;
+    gflags::SetUsageMessage("usage:");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     std::ifstream data(FLAGS_stream);
     std::string oneline;
@@ -43,48 +43,39 @@ int main(){
     int time=std::get<2>(social_actions.back());
 
     std::vector<SocialInfluence*> sc_infl;
+
     for(int i=0;i<FLAGS_n;i++){
         SocialInfluence *sg=new SocialInfluence();
         sc_infl.push_back(sg);
-    }
-    std::vector<double> fs;
-    std::map<int,int> u_t;
-    for(int r=0;r<FLAGS_R;r++){
-        //n times sampling
-        double f=0;
-        for(int i=0;i<FLAGS_n;i++){
-            for(auto &a:social_actions){
-                int u=std::get<0>(a).first;
-                int v=std::get<0>(a).second;
-                int timestamp=std::get<2>(a);
-                if(std::find(picked_users.begin(),picked_users.end(),u)!=picked_users.end()){
-                    u_t[u]= timestamp;
-                }
-                if(std::find(picked_users.begin(),picked_users.end(),v)!=picked_users.end()){
-                    u_t[v]= timestamp;
-                }
-                if(rng.uniform() <= get_prob(FLAGS_lmd,time,timestamp))
-                {
-                    sc_infl[i]->addSocialAction(u,v,timestamp);
-                }
-
+        for(auto &a:social_actions){
+            int u=std::get<0>(a).first;
+            int v=std::get<0>(a).second;
+            int c=std::get<1>(a);
+            int timestamp=std::get<2>(a);
+            if(rng.uniform() <= get_prob(FLAGS_lmd,time,timestamp))
+            {
+                sc_infl[i]->addSocialAction(u,v,c);
             }
+        }
+    }
+    int temp=1;
+    std::vector<std::tuple<int,double>> rst;
+    for(int k=1;k<=FLAGS_n;k+=1){
+        double f=0;
+        for(int i=0;i<k;i++){
             double value=sc_infl[i]->getReward(picked_users);
             f+=value;
         }
-        for(int i=0;i<FLAGS_n;i++){
-            sc_infl[i]->clear(true);
-        }
-        fs.push_back(f/FLAGS_n);
+        rst.emplace_back(temp,f/k);
+        temp++;
     }
-    for(auto &item:fs){
-        std::cout<<item<<std::endl;
-    }
-    double expect=get_expect(FLAGS_lmd,FLAGS_k,s_in, time,u_t);
-    std::cout<<expect<<std::endl;
-    double nrmse= cal_nrmse(fs,expect);
-    std::cout<<nrmse<<std::endl;
 
+    std::string ofnm = osutils::join(
+            FLAGS_dir,
+            "samples_n{}lmd{}k{}.dat"_format(FLAGS_n, FLAGS_lmd,FLAGS_k
+            ));
+    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\n");
+    gflags::ShutDownCommandLineFlags();
     return 0;
 }
 
--- src/samples.h
+++ src/samples.h
@@ -6,53 +6,9 @@
 #define INFLUENCERS_TRACKING_SAMPLES_H
 
 #include "stdafx.h"
-#include "social_influence.h"
-
-std::string into2bin(int n,int count){
-    std::string str="";
-    for(int i=count-1;i>=0;i--){
-        auto temp=(n>>i)&1;
-        str+=std::to_string(temp);
-    }
-    return str;
-}
 
 double get_prob(double lambda,int time,int ta){
     return exp(-lambda*(time-ta));
-}
-
-double cal_nrmse(std::vector<double> &s,double expect){
-    double sum=0;
-    for(auto item:s){
-        sum+= pow(item-expect,2);
-    }
-    double nrmse= sqrt(sum/s.size())/expect;
-    return nrmse;
-}
-
-double get_expect(const double lambda, int k, SocialInfluence &s_sin, int time, std::map<int,int> u_t){
-    double expect=0;
-    for(int i=1;i<pow(2, k); i++){
-        std::string t= into2bin(i, k);
-        double temp_prob=1;
-        auto iter=u_t.begin();
-        std::vector<int> u;
-        for(int j=0; j < k; j++){
-            int ex;
-            if(t[j]=='1'){
-                u.push_back(iter->first);
-                ex=1;
-            }else{
-                ex=0;
-            }
-            temp_prob=temp_prob*pow(get_prob(lambda,time,iter->second),ex)
-                      *pow(1-get_prob(lambda,time,iter->second), 1-ex);
-            iter++;
-        }
-        double reward=s_sin.getReward(u);
-        expect+=temp_prob*reward;
-    }
-    return expect;
 }
 
 #endif //INFLUENCERS_TRACKING_SAMPLES_H
