081ac610df436e5ec71f8bd6a02a494050de4c51
3650209 <1627982051@qq.com>
2021-06-22 11:25:07 +0800

    use template

--- src/greedy_basic.h
+++ src/greedy_basic.h
@@ -9,12 +9,15 @@
 #include "greedy_pait.h"
 #include "iset_segment.h"
 
+/**
+ * GreedyBasic
+ */
+template <typename Fun>
 class GreedyBasic{
 private:
     int L_,cur_=0;
-    std::vector<GreedyAlg*> greedy_ptrs_;
+    std::vector<GreedyAlg<Fun>*> greedy_ptrs_;
 public:
-    GreedyBasic(const int L,const int budget,const int num_samples);
     double getResult(std::vector<int> nodes);
 
     virtual ~GreedyBasic(){
@@ -23,12 +26,6 @@ public:
         }
     }
 
-    void update(const SocialAc &s,const ISetSegments& segs);
-
-    double getResult()const;
-
-    void next();
-
     int statOracleCalls(){
         int oracle_calls=0;
         for(int i=0;i<L_;i++){
@@ -36,36 +33,35 @@ public:
         }
         return oracle_calls;
     }
-};
 
-GreedyBasic::GreedyBasic(const int L,const int budget,const int num_samples){
-    L_=L;
-    greedy_ptrs_.resize(L);
-    for(int l=0;l<L_;l++){
-        greedy_ptrs_[l]=new GreedyAlg(num_samples,budget);
-    }
-}
-
-void GreedyBasic::update(const SocialAc &s,const ISetSegments& segs){
-    for(auto& seg:segs.segments_){
-        for(int i=seg.start_;i<seg.end_;i++){
-            greedy_ptrs_[(i+cur_)%L_]->update(s,seg.is_);
+    GreedyBasic(const int L,const int budget,const int num_samples){
+        L_=L;
+        greedy_ptrs_.resize(L);
+        for(int l=0;l<L_;l++){
+            greedy_ptrs_[l]=new GreedyAlg<Fun>(num_samples,budget);
         }
     }
-}
 
-void GreedyBasic::next(){
-    greedy_ptrs_[cur_]->clear(true);
-    for(int i=0;i<L_;i++){
-        greedy_ptrs_[i]->clear();
+    void update(const Action &a,const ISetSegments& segs){
+        for(auto& seg:segs.segments_){
+            for(int i=seg.start_;i<seg.end_;i++){
+                greedy_ptrs_[(i+cur_)%L_]->update(a,seg.is_);
+            }
+        }
     }
-    cur_=(cur_+1)%L_;
-}
 
-double GreedyBasic::getResult()const {
-    return greedy_ptrs_[cur_]->getResult();
-}
+    void next(){
+        greedy_ptrs_[cur_]->clear(true);
+        for(int i=0;i<L_;i++){
+            greedy_ptrs_[i]->clear();
+        }
+        cur_=(cur_+1)%L_;
+    }
 
+    double getResult()const {
+        return greedy_ptrs_[cur_]->getResult();
+    }
+};
 
 
 #endif //INFLUENCERS_TRACKING_GREEDY_BASIC_H
