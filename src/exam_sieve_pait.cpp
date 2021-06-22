e4d99b775e9b6bd8730be7cd33bb54d0c8f38211
3650209 <1627982051@qq.com>
2021-06-22 11:15:23 +0800

    test SievePAIT

--- src/exam_sieve_pait.cpp
+++ src/exam_sieve_pait.cpp
@@ -5,16 +5,16 @@
 #include "sieve_pait.h"
 #include "iset_generator.h"
 #include "stackexchange_obj_fun.h"
-
 #include <gflags/gflags.h>
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_string(obj, "output.txt", "objective file name");
 DEFINE_int32(n, 10, "number of samples");
 DEFINE_int32(B, 10, "budget");
 DEFINE_double(eps, 0.2, "epsilon");
 DEFINE_double(p, 0.6, "probability");
+DEFINE_int32(end,1000,"end time");
+
 
 int main(int argc, char* argv[]) {
     gflags::SetUsageMessage("usage:");
@@ -22,15 +22,15 @@ int main(int argc, char* argv[]) {
     osutils::Timer tm;
 
     SievePAIT<StackExObjFun> sieve(FLAGS_n, FLAGS_B, FLAGS_eps);
-
     ISetGenerator isgen(FLAGS_n, FLAGS_p);
 
     std::vector<std::tuple<int, double>> rst;
-    int temp = 1;
     ioutils::TSVParser ss(FLAGS_stream);
+    int t=0;
     while (ss.next()) {
-        int u = ss.get<int>(0), c = ss.get<int>(1), t = ss.get<int>(2);
-        Action a;  // TODO: create an social action using u,c,t,...
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
+        Action a{u,v,c,t};
 
         ISet iset = isgen.getISet();
         sieve.update(a, iset);
@@ -41,7 +41,8 @@ int main(int argc, char* argv[]) {
         std::cout << val << std::endl;
         //        std::cout<<num_calls<<std::endl;
         sieve.clear();
-        rst.emplace_back(temp, val);
+        rst.emplace_back(t, val);
+        if(t==FLAGS_end) break;
     }
 
     std::string ofnm = osutils::join(
