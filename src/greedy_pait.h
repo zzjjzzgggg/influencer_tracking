1857de493c25afac9772f2899b1f1e301bb0c618
3650209 <1627982051@qq.com>
2021-06-22 11:25:10 +0800

    use template

--- src/greedy_pait.h
+++ src/greedy_pait.h
@@ -6,84 +6,64 @@
 #define INFLUENCERS_TRACKING_GREEDY_PAIT_H
 
 #include "stdafx.h"
-#include "social_influence.h"
+#include "obj_mgr.h"
+#include "candidate.h"
 
+template<typename Fun>
 class GreedyAlg{
 public:
-    int budget_;     //k
-    int num_samples_;   //n
-    SocialInfluence sg_;
-    std::vector<SocialInfluence*> sam_graphs_;
+    int num_samples_,budget_;     //n and k
+    ObjMgr<Fun> obj_mgr_;       // maintains n objective functions
+    std::unordered_set<int> users_;
 
 public:
-    GreedyAlg(const int num_samples,const int budget);
-    void update(const SocialAc &a, const ISet& is);
+    GreedyAlg(const int num_samples,const int budget):num_samples_(num_samples), budget_(budget),obj_mgr_(num_samples_){
+    }
     double getResult();
     void clear(const bool deep=false){
-        sg_.clear(deep);
-        for(int i=0;i<num_samples_;i++){
-            sam_graphs_[i]->clear(deep);
+        obj_mgr_.clear(deep);
+        if(deep){
+            users_.clear();
         }
     }
-    int getOracleCalls(){
-        int oracle_calls=0;
-        for(int i=0;i<num_samples_;i++){
-            oracle_calls+=sam_graphs_[i]->getOracleCalls();
+    void update(const Action &a, const ISet &iset) {
+        //add social action
+        obj_mgr_.update(a, iset);
+        if(users_.find(a.u)==users_.end()){
+            users_.insert(a.u);
         }
-        return oracle_calls;
+        if(users_.find(a.v)==users_.end()){
+            users_.insert(a.v);
+        }
     }
+
+    int getOracleCalls() const { return obj_mgr_.getOracleCalls(); }
 };
-GreedyAlg::GreedyAlg(const int num_samples,const int budget)
-{
-    num_samples_=num_samples;
-    budget_=budget;
-    for(int i=0;i<num_samples;i++){
-        SocialInfluence *sg=new SocialInfluence();
-        sam_graphs_.push_back(sg);
-    }
-}
 
-void GreedyAlg::update(const SocialAc &a, const ISet &is) {
-    //add social action
-    sg_.addSocialAction(std::get<0>(a).first, std::get<0>(a).second, std::get<1>(a));
+template <typename Fun>
+double GreedyAlg<Fun>::getResult(){
 
-    for(auto i:is){
-        sam_graphs_[i]->addSocialAction(std::get<0>(a).first, std::get<0>(a).second, std::get<1>(a));
-    }
-}
-
-double GreedyAlg::getResult(){
     //get nodes
-    std::vector<int> nodes=sg_.getNodes();
+    std::unordered_set<int> users=users_;
 
-    double rwd_mx=0;
     std::vector<int> S;
-    int mx_gain_node;
+    int mx_gain_u;
     for(int i=0;i<budget_;i++){
         double gain_mx=-100;
-        for(auto &node:nodes){
-            double gain_sums=0;
-            for(int k=0;k<num_samples_;k++){
-                gain_sums+=sam_graphs_[k]->getGain(node,S);
-            }
-            double gain=gain_sums/num_samples_;
+        for(auto &u:users){
+            double gain=obj_mgr_.getGain(u,S);
             if(gain>gain_mx){
                 gain_mx=gain;
-                mx_gain_node=node;
+                mx_gain_u=u;
             }
         }
-        S.push_back(mx_gain_node);
-        nodes.erase(remove(nodes.begin(), nodes.end(), mx_gain_node), nodes.end());
-        if(nodes.empty()){
+        S.push_back(mx_gain_u);
+        users.erase(mx_gain_u);
+        if(users.empty()){
             break;
         }
     }
-    double gains_all=0;
-
-    for(int i=0;i<num_samples_;i++){
-        gains_all+=sam_graphs_[i]->getReward(S);
-    }
-    rwd_mx=gains_all/num_samples_;
+    double rwd_mx=obj_mgr_.getVal(S);
     return  rwd_mx;
 }
 
