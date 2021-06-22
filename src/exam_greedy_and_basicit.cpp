21253f4972f6248c575c939fa05917ad0473485a
3650209 <1627982051@qq.com>
2021-06-22 11:47:24 +0800

    test greedy and BasicIT

--- src/exam_greedy_and_basicit.cpp
+++ src/exam_greedy_and_basicit.cpp
@@ -32,8 +32,8 @@ int main(int argc, char* argv[]){
     double ritio_sums=0;
     while(ss.next()){
         ++t;
-        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
-        Action a{u,v,c,t};
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
+        Action a{u,v,c,ta};
 
         std::vector<int> lifespan=lifegen.getLifespans(FLAGS_n);
         ISetSegments segs(lifespan);
@@ -63,7 +63,7 @@ int main(int argc, char* argv[]){
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "basic_and_greedy_n{}b{}eps{}lmd{}L{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_eps,FLAGS_lmd,FLAGS_L));
-    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\t{}\n");
+    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
     gflags::ShutDownCommandLineFlags();
     return 0;
 }
