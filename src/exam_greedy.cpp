9c5e317e9d9ec262c3301d2e951dfb8952f455ed
3650209 <1627982051@qq.com>
2021-06-22 11:20:02 +0800

    test greedy

--- src/exam_greedy.cpp
+++ src/exam_greedy.cpp
@@ -6,55 +6,43 @@
 #include "greedy_pait.h"
 #include "iset_generator.h"
 #include <gflags/gflags.h>
+#include "stackexchange_obj_fun.h"
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_string(obj, "output.txt", "objective file name");
 DEFINE_int32(n, 10, "number of samples");
 DEFINE_int32(B, 10, "budget");
 DEFINE_double(p, 0.6, "probability");
+DEFINE_int32(end,1000,"end time");
 
 int main(int argc, char* argv[]){
     gflags::SetUsageMessage("usage:");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     osutils::Timer tm;
 
-    GreedyAlg greedy{FLAGS_n,FLAGS_B};
+    GreedyAlg<StackExObjFun> greedy{FLAGS_n,FLAGS_B};
     ISetGenerator isgen(FLAGS_n,FLAGS_p);
 
-    std::ifstream data(FLAGS_stream);
-    std::string oneline;
-    int x=0;
-
-    SocialAcs social_actions;
-    while(getline(data,oneline)){
-        std::istringstream read_str(oneline);
-        int item;
-        std::vector<int> temp;
-        while(read_str>>item)
-            temp.push_back(int(item));
-        social_actions.emplace_back(std::make_pair(temp[1],temp[2]),temp[0],temp[3]);
-        x++;
-        if(x==100)
-            break;
-    }
-
-    int temp=1;
+    ioutils::TSVParser ss(FLAGS_stream);
     std::vector<std::tuple<int,double>> rst;
-    std::ofstream out(FLAGS_obj);
-    for(auto &a:social_actions){
-        ISet is=isgen.getISet();
-        greedy.update(a,is);
+    int t=0;
+    while(ss.next()){
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
+        Action a{u,v,c,t};
+        ISet iset=isgen.getISet();
 
-        double greedy_gain=greedy.getResult();
-        int g_ocalls=greedy.getOracleCalls();
+        greedy.update(a,iset);
+        double val=greedy.getResult();
+//        int num_ocalls=greedy.getOracleCalls();
 
-        std::cout<<"greedy:"<<greedy_gain<<std::endl;
-        std::cout<<g_ocalls<<std::endl;
+        std::cout<<"greedy:"<<val<<std::endl;
+//        std::cout<<num_ocalls<<std::endl;
         greedy.clear();
-        rst.emplace_back(temp,greedy_gain);
-        temp++;
+        rst.emplace_back(t,val);
+        if(t==FLAGS_end) break;
     }
+
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "greedy_n{}b{}p{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_p));
