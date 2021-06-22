cd88ddcbc333ac1e17675ad35e57719be31aa04c
3650209 <1627982051@qq.com>
2021-06-22 11:47:44 +0800

    test greedy and SievePAIT

--- src/exam_greedy_and_pait.cpp
+++ src/exam_greedy_and_pait.cpp
@@ -31,8 +31,8 @@ int main(int argc, char* argv[]){
     double sum=0;
     while(ss.next()){
         ++t;
-        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
-        Action a{u,v,c,t};
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
+        Action a{u,v,c,ta};
 
         ISet iset=isgen.getISet();
         pait.update(a,iset);
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]){
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "pait_and_greedy_n{}b{}p{}eps{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_p,FLAGS_eps));
-    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\t{}\n");
+    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
     gflags::ShutDownCommandLineFlags();
     return 0;
 }
