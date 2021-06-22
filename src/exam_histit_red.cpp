f9d69e0a037f5a844952943615b42c77fc18c67a
3650209 <1627982051@qq.com>
2021-06-22 11:22:33 +0800

    test HistITRED

--- src/exam_histit_red.cpp
+++ src/exam_histit_red.cpp
@@ -4,64 +4,54 @@
 
 #include "histit_seg.h"
 #include "histit_red.h"
-#include "iset_segment.h"
 #include "lifespan_generator.h"
 #include <gflags/gflags.h>
+#include "stackexchange_obj_fun.h"
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_string(obj, "output.txt", "objective file name");
 DEFINE_int32(n, 10, "number of samples");
 DEFINE_int32(B, 10, "budget");
 DEFINE_double(eps, 0.2, "epsilon");
 DEFINE_double(lmd, .01, "decaying rate");
 DEFINE_int32(L, 40, "maximum lifetime");
+DEFINE_int32(end,1000,"end time");
+
 
 int main(int argc, char* argv[]){
     gflags::SetUsageMessage("usage:");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
+    osutils::Timer tm;
 
     LifespanGenerator lifegen(FLAGS_L,1-exp(-FLAGS_lmd));
-    HistITSEG hist(FLAGS_n,FLAGS_B,FLAGS_eps);
-    HistITRED hist_red(FLAGS_n,FLAGS_B,FLAGS_eps);
-
-    std::string oneline;
-    int x=0;
-    std::ifstream data(FLAGS_stream);
-    SocialAcs social_actions;
-    while(getline(data,oneline)){
-        std::istringstream read_str(oneline);
-        int item;
-        std::vector<int> temp;
-        while(read_str>>item)
-            temp.push_back(int(item));
-        social_actions.emplace_back(std::make_pair(temp[1],temp[2]),temp[0],temp[3]);
-        x++;
-        if(x==5000)
-            break;
-    }
-
-    int temp=1;
+    HistITSEG<StackExObjFun> hist(FLAGS_n,FLAGS_B,FLAGS_eps);
+    HistITRED<StackExObjFun> hist_red(FLAGS_n,FLAGS_B,FLAGS_eps);
 
     std::vector<std::tuple<int,double,double>> rst;
-    for(auto &a:social_actions){
+    ioutils::TSVParser ss(FLAGS_stream);
+    int t=0;
+    while (ss.next()) {
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
+        Action a{u,v,c,t};
+
         std::vector<int> lifespan=lifegen.getLifespans(FLAGS_n);
         ISetSegments segs(lifespan);
 
         hist.feed(a,segs);
-        std::cout<<temp<<" ";
-        double hist_mx=hist.getResult();
-        std::cout<<"hist"<<hist_mx<<" ";
+        double hist_val=hist.getResult();
+        std::cout<<t<<" ";
+        std::cout << "hist" << hist_val << " ";
         hist.next();
 
         hist_red.feed(a, segs);
-        double hist_red_mx=hist_red.getResult();
-        std::cout<<"histred"<<hist_red_mx<<std::endl;
+        double hist_red_val=hist_red.getResult();
+        std::cout << "histred" << hist_red_val << std::endl;
 
         hist_red.reduce();
         hist_red.next();
-        rst.emplace_back(temp,hist_mx,hist_red_mx);
-        temp++;
+        rst.emplace_back(t, hist_val, hist_red_val);
+        if(t==FLAGS_end) break;
     }
     std::string ofnm = osutils::join(
             FLAGS_dir,
