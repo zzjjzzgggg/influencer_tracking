a68bcfdd0501cdacaaa727e38e96ca3dbf50deb7
3650209 <1627982051@qq.com>
2021-06-22 11:48:18 +0800

    test HistITRED

--- src/exam_histit_red.cpp
+++ src/exam_histit_red.cpp
@@ -32,8 +32,8 @@ int main(int argc, char* argv[]){
     int t=0;
     while (ss.next()) {
         ++t;
-        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
-        Action a{u,v,c,t};
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
+        Action a{u,v,c,ta};
 
         std::vector<int> lifespan=lifegen.getLifespans(FLAGS_n);
         ISetSegments segs(lifespan);
@@ -56,7 +56,8 @@ int main(int argc, char* argv[]){
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "hist_and_histred_n{}b{}eps{}lmd{}L{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_eps,FLAGS_lmd,FLAGS_L));
-    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\t{}\n");
+    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
+    printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
     return 0;
 }
