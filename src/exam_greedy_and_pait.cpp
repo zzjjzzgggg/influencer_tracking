e374cc70a5dbf593783b26faea6a5c3a9b5e746e
3650209 <1627982051@qq.com>
2021-06-22 11:20:19 +0800

    test greedy and SievePAIT

--- src/exam_greedy_and_pait.cpp
+++ src/exam_greedy_and_pait.cpp
@@ -2,75 +2,61 @@
 // Created by weiwei on 2021/5/30.
 //
 
+#include "greedy_pait.h"
 #include "sieve_pait.h"
 #include "iset_generator.h"
 #include <gflags/gflags.h>
-#include "greedy_pait.h"
+#include "stackexchange_obj_fun.h"
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_string(obj, "output.txt", "objective file name");
 DEFINE_int32(n, 10, "number of samples");
 DEFINE_int32(B, 10, "budget");
 DEFINE_double(eps, 0.2, "epsilon");
 DEFINE_double(p, 0.6, "probability");
+DEFINE_int32(end,1000,"end time");
 
 int main(int argc, char* argv[]){
     gflags::SetUsageMessage("usage:");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
+    osutils::Timer tm;
 
-    SievePAIT pait(FLAGS_n,FLAGS_B,FLAGS_eps);
+    SievePAIT<StackExObjFun> pait(FLAGS_n,FLAGS_B,FLAGS_eps);
+    GreedyAlg<StackExObjFun> greedy{FLAGS_n,FLAGS_B};
     ISetGenerator isgen(FLAGS_n,FLAGS_p);
-    GreedyAlg greedy{FLAGS_n,FLAGS_B};
 
-    SocialAcs social_actions;
-
-    std::ifstream data(FLAGS_stream);
-    std::string oneline;
-    int x=0;
-    while(getline(data,oneline)){
-        std::istringstream read_str(oneline);
-        int item;
-        std::vector<int> temp;
-        while(read_str>>item)
-            temp.push_back(int(item));
-        social_actions.emplace_back(std::make_pair(temp[1],temp[2]),temp[0],temp[3]);
-        x++;
-        if(x==100)
-            break;
-    }
-
-    int temp=1;
+    ioutils::TSVParser ss(FLAGS_stream);
+    int t=0;
     std::vector<std::tuple<int,double,double>> rst;
-    std::ofstream out(FLAGS_obj);
     double sum=0;
-    double ritio=0;
-    for(auto &s:social_actions){
-        ISet is=isgen.getISet();
-        pait.update(s,is);
-        double pait_mx=pait.getResult();
-        std::cout<<temp<<" ";
-        std::cout<<"sieve_pait"<<pait_mx<<"  ";
+    while(ss.next()){
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), t= ss.get<int>(3);
+        Action a{u,v,c,t};
+
+        ISet iset=isgen.getISet();
+        pait.update(a,iset);
+
+        double pait_val=pait.getResult();
+        std::cout<<t<<" ";
+        std::cout<<"sieve_pait"<<pait_val<<"  ";
         int p_ocalls=pait.getOracleCalls();
 
         pait.clear();
 
-        greedy.update(s,is);
-        double greedy_gain=greedy.getResult();
+        greedy.update(a,iset);
+        double greedy_val=greedy.getResult();
 
-        std::cout<<"greedy:"<<greedy_gain<<std::endl;
+        std::cout<<"greedy:"<<greedy_val<<std::endl;
         int g_ocalls=greedy.getOracleCalls();
 
 //        std::cout<<" "<<g_ocalls<<std::endl;
         greedy.clear();
-        sum+=pait_mx/greedy_gain;
-        rst.emplace_back(temp,pait_mx,greedy_gain);
-        ritio+=p_ocalls/g_ocalls;
-
-        temp++;
+        sum+=pait_val/greedy_val;
+        rst.emplace_back(t,pait_val,greedy_val);
+        if(t==FLAGS_end) break;
     }
-    std::cout<<ritio/temp<<std::endl;
-    std::cout<<sum/x<<std::endl;
+    std::cout<<sum/t<<std::endl;
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "pait_and_greedy_n{}b{}p{}eps{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_p,FLAGS_eps));
