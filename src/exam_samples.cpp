9d48f28cb9a5cfc9066b8fe355727921fbc299ef
3650209 <1627982051@qq.com>
2021-06-22 19:39:48 +0800

    test samples

--- src/exam_samples.cpp
+++ src/exam_samples.cpp
@@ -2,76 +2,57 @@
 // Created by weiwei on 2021/6/7.
 //
 
-#include "social_influence.h"
+#include "stackexchange_obj_fun.h"
 #include <gflags/gflags.h>
 #include "samples.h"
+#include "obj_mgr.h"
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
 DEFINE_int32(n, 2000, "number of max samples");
-DEFINE_int32(i,1,"experiment index");
+DEFINE_int32(i,3,"experiment index");
 DEFINE_double(lmd, .01, "decaying rate");
 DEFINE_int32(k,10,"number of picked users");
+DEFINE_double(p, 0.6, "probability");
+DEFINE_int32(end,1000,"end time");
 
 
 int main(int argc, char* argv[]){
     rngutils::default_rng rng;
     gflags::SetUsageMessage("usage:");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
+    osutils::Timer tm;
 
-    std::ifstream data(FLAGS_stream);
-    std::string oneline;
-    int x=0;
-    SocialAcs social_actions;
-    while(getline(data,oneline)){
-        std::istringstream read_str(oneline);
-        int item;
-        std::vector<int> temp;
-        while(read_str>>item)
-            temp.push_back(int(item));
-        social_actions.emplace_back(std::make_pair(temp[1],temp[2]),temp[0],temp[3]);
-        x++;
-        if(x==1000)
-            break;
-    }
+    ObjMgr<StackExObjFun> obj(FLAGS_n);
+    ioutils::TSVParser ss(FLAGS_stream);
+    int t=0;
+    std::vector<int> users;
+    while (ss.next()){
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
+        Action a{u,v,c,ta};
+        std::vector<int> iset;
+        for(int k=0;k<FLAGS_n;k++){
+            if(rng.uniform() <= get_prob(FLAGS_lmd,FLAGS_end,ta)){
+                iset.push_back(k);
+            }
+        }
+        obj.update(a,iset);
 
-    SocialInfluence s_in;
-    for(auto &a:social_actions){
-        s_in.addSocialAction(std::get<0>(a).first,std::get<0>(a).second,std::get<1>(a));
+        if(std::find(users.begin(),users.end(),u)==users.end()){
+            users.push_back(u);
+        }
+        if(std::find(users.begin(),users.end(),v)==users.end()){
+            users.push_back(v);
+        }
+        if(t==FLAGS_end) break;
     }
 
     std::vector<int> picked_users;
-    picked_users=rngutils::choose(s_in.users_, FLAGS_k, rng);
-    int time=std::get<2>(social_actions.back());
+    picked_users=rngutils::choose(users, FLAGS_k, rng);
 
     //the approximate true value
-    double fts_true;
-    double f=0;
-    double repeat_num=100;
-    //get mean Ft(S)
-    for(int i=0;i<repeat_num;i++){
-        //n stream samples
-        double fts_sum=0;
-        for(int k=0;k<FLAGS_n;k++){
-            SocialInfluence *sg=new SocialInfluence();
-            for(auto &a:social_actions){
-                int u=std::get<0>(a).first;
-                int v=std::get<0>(a).second;
-                int c=std::get<1>(a);
-                int timestamp=std::get<2>(a);
-                if(rng.uniform() <= get_prob(FLAGS_lmd,time,timestamp))
-                {
-                    sg->addSocialAction(u,v,c);
-                }
-            }
-            fts_sum += sg->getReward(picked_users);
-            delete sg;
-            sg= nullptr;
-        }
-        f+=fts_sum/FLAGS_n;
-    }
-    fts_true=f/repeat_num;
-
+    double fts_true=obj.getVal(picked_users);
     std::cout<<fts_true<<std::endl;
 
     int sample_size=1000;
@@ -79,41 +60,35 @@ int main(int argc, char* argv[]){
     std::vector<std::tuple<int,double>> rst;
     for(int i=10;i<=sample_size;i+=10){
         std::vector<double> fts_set;
-        double s=0;
-        for(int j=0;j<r;j++){
-            double f_sum=0;
-            for(int m=0;m<i;m++) {
-                SocialInfluence *sg = new SocialInfluence();
-                for(auto &a:social_actions){
-                    int u=std::get<0>(a).first;
-                    int v=std::get<0>(a).second;
-                    int c=std::get<1>(a);
-                    int timestamp=std::get<2>(a);
-                    if(rng.uniform() <= get_prob(FLAGS_lmd,time,timestamp))
-                    {
-                        sg->addSocialAction(u,v,c);
+        for(int j=1;j<=r;j++){
+            int temp=0;
+            ObjMgr<StackExObjFun> objmgr(i);
+            ioutils::TSVParser ss1(FLAGS_stream);
+            while (ss1.next()&&(++temp)<=1000){
+                int c = ss1.get<int>(0), u = ss1.get<int>(1), v=ss1.get<int>(2), ta= ss1.get<int>(3);
+                Action a{u,v,c,ta};
+                std::vector<int> iset;
+                for(int k=0;k<i;k++){
+                    if(rng.uniform() <= get_prob(FLAGS_lmd,FLAGS_end,ta)){
+                        iset.push_back(k);
                     }
                 }
-                f_sum+=sg->getReward(picked_users);
-                delete sg;
-                sg= nullptr;
+                objmgr.update(a,iset);
             }
-            //ft_s value
-            double f=f_sum/i;
-            fts_set.push_back(f);
+            double val=objmgr.getVal(picked_users);
+            fts_set.push_back(val);
         }
         double mse= get_mse(fts_set,fts_true);
         double nrmse= sqrt(mse)/fts_true;
         rst.emplace_back(i,nrmse);
     }
-//    for(auto item:rst){
-//        std::cout<<std::get<0>(item)<<" "<<std::get<1>(item)<<std::endl;
-//    }
+
     std::string ofnm = osutils::join(
             FLAGS_dir,
             "samples_n{}lmd{}k{}i{}.dat"_format(FLAGS_n, FLAGS_lmd,FLAGS_k,FLAGS_i
             ));
     ioutils::saveTupleVec(rst, ofnm, "{}\t{}\n");
+    printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
     return 0;
 }
