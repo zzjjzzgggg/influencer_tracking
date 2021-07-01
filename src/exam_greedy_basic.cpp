ff0b6f4af6c4265b0dea0791efbc8b780a596e5c
3650209 <1627982051@qq.com>
2021-07-01 11:05:59 +0800

    test greedyBasic

--- src/exam_greedy_basic.cpp
+++ src/exam_greedy_basic.cpp
@@ -8,11 +8,12 @@
 #include "stackexchange_obj_fun.h"
 
 DEFINE_string(dir, "", "working directory");
-DEFINE_string(stream, "comment_post.txt", "input streaming data file name");
-DEFINE_int32(n, 10, "number of samples");
-DEFINE_int32(B, 10, "budget");
+DEFINE_string(stream, "stackexchange.txt", "input streaming data file name");
+DEFINE_string(lifespans, "../../lifespans/lmd{:g}n{}L{}.gz", "lifespans template");
+DEFINE_int32(n, 50, "number of samples");
+DEFINE_int32(B, 20, "budget");
 DEFINE_double(lmd, .01, "decaying rate");
-DEFINE_int32(L, 40, "maximum lifetime");
+DEFINE_int32(L, 100, "maximum lifetime");
 DEFINE_int32(end,1000,"end time");
 
 int main(int argc, char* argv[]){
@@ -21,32 +22,38 @@ int main(int argc, char* argv[]){
     osutils::Timer tm;
 
     GreedyBasic<StackExObjFun> greedy(FLAGS_L,FLAGS_B,FLAGS_n);
-    LifespanGenerator lifegen(FLAGS_L,1-exp(-FLAGS_lmd));
-
+    std::string lifespan_fnm =
+            osutils::join(FLAGS_dir, fmt::format(FLAGS_lifespans, FLAGS_lmd, FLAGS_n,
+                                                 strutils::prettyNumber(FLAGS_L)));
+    auto pin = ioutils::getIOIn(lifespan_fnm);
+    std::vector<int> lifespans;
+    std::vector<std::tuple<int,double,int>> rst;
     ioutils::TSVParser ss(FLAGS_stream);
-    std::vector<std::tuple<int,double>> rst;
-    int t=0;
+    int t=0,ocalls=0;
     while(ss.next()){
         ++t;
-        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
-        Action a{u,v,c,ta};
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2);
+        Action a{u,v,c,t};
 
-        std::vector<int> lifespan=lifegen.getLifespans(FLAGS_n);
-        ISetSegments segs(lifespan);
+        lifespans.clear();
+        pin->load(lifespans);
+        ISetSegments segs(lifespans);
+
         greedy.update(a,segs);
 
         double val=greedy.getResult();
+        ocalls+=greedy.statOracleCalls();
         std::cout<<val<<std::endl;
 
-        rst.emplace_back(t,val);
+        rst.emplace_back(t,val,ocalls);
         greedy.next();
         if(t==FLAGS_end) break;
     }
 
     std::string ofnm = osutils::join(
             FLAGS_dir,
-            "greedy_basic_n{}b{}lmd{}L{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_L,FLAGS_L));
-    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\n");
+            "greedy_basic_n{}b{}lmd{}L{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_lmd,FLAGS_L));
+    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
     printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
     return 0;
