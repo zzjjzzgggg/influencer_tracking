c102f860ed08c03b14e3327a6279a82ef444af1d
3650209 <1627982051@qq.com>
2021-07-02 17:53:49 +0800

    test greedy and sievepait

--- src/exam_greedy_and_pait.cpp
+++ src/exam_greedy_and_pait.cpp
@@ -2,19 +2,20 @@
 // Created by weiwei on 2021/5/30.
 //
 
-#include "greedy_pait.h"
+#include "simple_greedy.h"
 #include "sieve_pait.h"
 #include "iset_generator.h"
 #include <gflags/gflags.h>
 #include "stackexchange_obj_fun.h"
+#include "obj_mgr.h"
 
 DEFINE_string(dir, "", "working directory");
 DEFINE_string(stream, "stackexchange.txt", "input streaming data file name");
-DEFINE_int32(n, 10, "number of samples");
+DEFINE_int32(n, 50, "number of samples");
 DEFINE_int32(B, 10, "budget");
 DEFINE_double(eps, 0.2, "epsilon");
 DEFINE_double(p, 0.6, "probability");
-DEFINE_int32(end,1000,"end time");
+DEFINE_int32(T,10000,"end time");
 
 int main(int argc, char* argv[]){
     gflags::SetUsageMessage("usage:");
@@ -22,7 +23,8 @@ int main(int argc, char* argv[]){
     osutils::Timer tm;
 
     SievePAIT<StackExObjFun> pait(FLAGS_n,FLAGS_B,FLAGS_eps);
-    GreedyAlg<StackExObjFun> greedy{FLAGS_n,FLAGS_B};
+    ObjMgr<StackExObjFun> obj(FLAGS_n);
+    SimpleGreedy<StackExObjFun> greedy(&obj, FLAGS_B);
     ISetGenerator isgen(FLAGS_n,FLAGS_p);
 
     ioutils::TSVParser ss(FLAGS_stream);
@@ -31,8 +33,8 @@ int main(int argc, char* argv[]){
     double sum=0;
     while(ss.next()){
         ++t;
-        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2), ta= ss.get<int>(3);
-        Action a{u,v,c,ta};
+        int c = ss.get<int>(0), u = ss.get<int>(1), v=ss.get<int>(2);
+        Action a{u,v,c,t};
 
         ISet iset=isgen.getISet();
         pait.update(a,iset);
@@ -40,26 +42,24 @@ int main(int argc, char* argv[]){
         double pait_val=pait.getResult();
         std::cout<<t<<" ";
         std::cout<<"sieve_pait"<<pait_val<<"  ";
-        int p_ocalls=pait.getOracleCalls();
 
         pait.clear();
 
-        greedy.update(a,iset);
-        double greedy_val=greedy.getResult();
+        obj.update(a,iset);
+        double greedy_val=greedy.run();
+        obj.clear();
 
         std::cout<<"greedy:"<<greedy_val<<std::endl;
-        int g_ocalls=greedy.getOracleCalls();
 
-//        std::cout<<" "<<g_ocalls<<std::endl;
-        greedy.clear();
         sum+=pait_val/greedy_val;
         rst.emplace_back(t,pait_val,greedy_val);
-        if(t==FLAGS_end) break;
+        if(t==FLAGS_T) break;
     }
     std::cout<<sum/t<<std::endl;
     std::string ofnm = osutils::join(
             FLAGS_dir,
-            "pait_and_greedy_n{}b{}p{}eps{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_p,FLAGS_eps));
+            "pait_and_greedy_n{}b{}p{}eps{}.dat"_format(FLAGS_n, FLAGS_B,FLAGS_p,
+                    FLAGS_eps, strutils::prettyNumber(FLAGS_T)));
     ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
     printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
