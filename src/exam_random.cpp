bdfdcd335a568b37d59a3092daa76cc04644ca3c
3650209 <1627982051@qq.com>
2021-07-02 17:50:30 +0800

    test random

---
+++ src/exam_random.cpp
@@ -0,0 +1,82 @@
+//
+// Created by weiwei on 2021/7/1.
+//
+
+#include "iset_segment.h"
+#include "eval_stream.h"
+#include "stackexchange_obj_fun.h"
+#include <gflags/gflags.h>
+
+DEFINE_string(dir, "", "working directory");
+DEFINE_string(stream, "stackexchange.txt", "input streaming data file name");
+DEFINE_string(lifespans, "../../lifespans/lmd{:g}n{}L{}.gz", "lifespans template");
+DEFINE_int32(n, 50, "number of samples");
+DEFINE_int32(B, 20, "budget");
+DEFINE_double(lmd, .01, "decaying rate");
+DEFINE_int32(L, 100, "maximum lifetime");
+DEFINE_int32(T,1000,"end time");
+
+
+int main(int argc, char* argv[]) {
+    gflags::SetUsageMessage("usage:");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    osutils::Timer tm;
+
+    rngutils::default_rng rng;
+    EvalStream<StackExObjFun> eval(FLAGS_L);
+
+    std::string lifespan_fnm =
+            osutils::join(FLAGS_dir, fmt::format(FLAGS_lifespans, FLAGS_lmd, FLAGS_n,
+                                                 strutils::prettyNumber(FLAGS_L)));
+    auto pin = ioutils::getIOIn(lifespan_fnm);
+    std::vector<int> lifespans;
+    ioutils::TSVParser ss(FLAGS_stream);
+
+    std::vector<std::tuple<int,double>> rst;
+    int t = 0;
+    while (ss.next()) {
+        ++t;
+        int c = ss.get<int>(0), u = ss.get<int>(1), v = ss.get<int>(2);
+        Action a{u, v, c, t};
+
+        lifespans.clear();
+        pin->load(lifespans);
+        ISetSegments segs(lifespans);
+
+        eval.add(a,segs);
+        auto pop = eval.getPop();
+        auto obj_mgr=eval.getObjMgr(FLAGS_n);
+
+        std::map<int,Action> this_actions=eval.get_Actions();
+        eval.next();
+
+        std::vector<int> users;
+        double val;
+        for (auto& pr : pop) {
+            int u=this_actions[pr.first].u;
+            int v=this_actions[pr.first].v;
+            if(std::find(users.begin(),users.end(),u)==users.end())
+                users.push_back(u);
+            if(std::find(users.begin(),users.end(),v)==users.end())
+                users.push_back(v);
+        }
+        if(users.size()>FLAGS_B){
+            auto samples=rngutils::choose(users, FLAGS_B, rng);
+            val=obj_mgr.getVal(samples);
+        }else{
+            val=obj_mgr.getVal(users);
+        }
+        rst.emplace_back(t,val);
+        if(t==FLAGS_T) break;
+    }
+
+    std::string ofnm =
+            osutils::join(FLAGS_dir, "random_lmd{:g}n{}k{}L{}T{}.dat"_format(
+                    FLAGS_lmd, FLAGS_n, FLAGS_B,strutils::prettyNumber(FLAGS_L),
+                    strutils::prettyNumber(FLAGS_T)));
+    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\n");
+    printf("cost time %s\n", tm.getStr().c_str());
+    gflags::ShutDownCommandLineFlags();
+    return 0;
+}
+
