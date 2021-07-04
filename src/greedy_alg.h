b4aecc959641796e1136dba48e159e118e066fd4
3650209 <1627982051@qq.com>
2021-07-04 17:40:03 +0800

    modify run

--- src/greedy_alg.h
+++ src/greedy_alg.h
@@ -42,15 +42,24 @@ public:
      * Run Greedy Algorithm on data provided by ObjMgr.
      * Return reward.
      */
-    double run(const std::unordered_map<int, ISet>& isets) {
+    double run(const std::unordered_map<int, ISet>& isets,std::map<int,Action> this_action) {
         chosen_.clear();
         auto cmp = [](Elem& a, Elem& b) { return a.gain < b.gain; };
         std::priority_queue<Elem, std::vector<Elem>, decltype(cmp)> pq(cmp);
 
-        for (auto& u:obj_ptr_->getUsers()) {
-            pq.emplace(u);
+        std::unordered_set<int> users;
+        for(auto item:isets){
+            int u=this_action[item.first].u;
+            int v=this_action[item.first].v;
+            if(users.find(u)==users.end()){
+                users.insert(u);
+                pq.emplace(u);
+            }
+            if(users.find(v)==users.end()){
+                users.insert(v);
+                pq.emplace(v);
+            }
         }
-
         double rwd = 0;
         int t = 1;
         while (!pq.empty() && t <= budget_) {
