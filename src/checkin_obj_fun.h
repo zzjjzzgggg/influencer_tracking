190e3ed423bca490ba66d4c9115fa13069e1d063
3650209 <1627982051@qq.com>
2022-01-28 04:17:04 +0800

    data structure for check in data

--- src/checkin_obj_fun.h
+++ src/checkin_obj_fun.h
@@ -13,14 +13,77 @@
  */
 class CheckinObjFun : public ObjFun {
 public:
-    CheckinObjFun();
+    mutable int oracle_calls_=0;
+    std::unordered_set<int> affected_;
+    std::unordered_map<int,std::unordered_set<int>> loc_sigma_;
 
-    void add(const Action& a);
-    void clear();
+public:
+    CheckinObjFun(){}
+    CheckinObjFun(const CheckinObjFun &o):affected_(o.affected_),oracle_calls_(0),
+                                            loc_sigma_(o.loc_sigma_){}
+    void add(const Action& a){
+        if(loc_sigma_.find(a.u)==loc_sigma_.end()){
+            //if location u is a new location
+            affected_.insert(a.u);
+            loc_sigma_[a.u].insert(a.v);
+        }else if(loc_sigma_[a.u].find(a.v)==loc_sigma_[a.u].end()){
+            //else if v is not included in location u's influence scope
+            affected_.insert(a.u);
+            loc_sigma_[a.u].insert(a.v);
+        }
+    }
 
-    double getVal(const int u) const;
-    double getVal(const std::vector<int>& S) const;
-    double getGain(const int v, const std::vector<int>& S) const;
+    // If deep = true, clean every thing; this is used when reseting SievePAIT
+    // is need in BasicIT
+    void clear(const bool deep=false){
+        oracle_calls_=0;
+        affected_.clear();
+        if(deep) loc_sigma_.clear();
+    }
+
+    //Get the reward of a location u.
+    double getVal(const int u) const{
+        if(loc_sigma_.find(u)==loc_sigma_.end())return 0;
+        return loc_sigma_.at(u).size();
+    }
+
+    //Get the reward of a location set.
+    double getVal(const std::vector<int>& S) const{
+        std::unordered_set<int> scope;
+        for(int u:S){
+            if(loc_sigma_.find(u)!=loc_sigma_.end()){
+                const auto& set =loc_sigma_.at(u);
+                scope.insert(set.begin(),set.end());
+            }
+        }
+
+        return scope.size();
+    }
+
+    // Get the gain of adding a location v to a location set S.
+    double getGain(const int v, const std::vector<int>& S) const{
+        if(loc_sigma_.find(v)==loc_sigma_.end())return 0;
+        ++oracle_calls_;
+
+        std::unordered_set<int> scope;
+
+        // firstly, calculate scope = sigma(S)
+        for (int u : S) {
+            if (loc_sigma_.find(u) != loc_sigma_.end()) {
+                const auto& set = loc_sigma_.at(u);
+                scope.insert(set.begin(), set.end());
+            }
+        }
+
+        int val = scope.size();
+
+        // secondly, calculate scope = sigma(S U {v})
+        const auto& set = loc_sigma_.at(v);
+        scope.insert(set.begin(), set.end());
+
+        // finally, calculate gain
+        return scope.size() - val;
+    }
 
 }; /* CheckinObjFun */
 
