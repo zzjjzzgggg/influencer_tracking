0c05662fb41a284db4f39c8d475a2c6f9b3a4d7a
3650209 <1627982051@qq.com>
2022-01-27 20:46:46 +0800

    exam histit-red

--- src/exam_histit_red.cpp
+++ src/exam_histit_red.cpp
@@ -4,18 +4,19 @@
 
 #include "histit_red.h"
 #include <gflags/gflags.h>
-#include "stackexchange_obj_fun.h"
+//#include "stackexchange_obj_fun.h"
+#include "checkin_obj_fun.h"
 
 DEFINE_string(dir, "", "working directory");
-DEFINE_string(stream, "stackexchange.txt", "input streaming data file name");
+DEFINE_string(stream, "brightkite.txt", "input streaming data file name");
 DEFINE_string(lifespans, "../../lifespans/lmd{:g}n{}L{}.gz", "lifespans template");
 DEFINE_int32(n, 50, "number of samples");
 DEFINE_int32(B, 20, "budget");
 DEFINE_double(sieve_eps,0.1,"sievepait epsilon");
 DEFINE_double(red_eps, 0.1, "histit_red epsilon");
 DEFINE_double(lmd, 0.002, "decaying rate");
-DEFINE_int32(L, 1000, "maximum lifetime");
-DEFINE_int32(T,100,"end time");
+DEFINE_int32(L, 100000, "maximum lifetime");
+DEFINE_int32(T,101,"end time");
 
 
 int main(int argc, char* argv[]){
@@ -23,14 +24,16 @@ int main(int argc, char* argv[]){
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     osutils::Timer tm;
 
-    HistITRED<StackExObjFun> hist_red(FLAGS_n,FLAGS_B,FLAGS_red_eps,FLAGS_sieve_eps);
+    HistITRED<CheckinObjFun> hist_red(FLAGS_n,FLAGS_B,FLAGS_red_eps,FLAGS_sieve_eps);
+
+//    HistITRED<StackExObjFun> hist_red(FLAGS_n,FLAGS_B,FLAGS_red_eps,FLAGS_sieve_eps);
     std::string lifespan_fnm =
             osutils::join(FLAGS_dir, fmt::format(FLAGS_lifespans, FLAGS_lmd, FLAGS_n,
                                                  strutils::prettyNumber(FLAGS_L)));
 
     auto pin = ioutils::getIOIn(lifespan_fnm);
     std::vector<int> lifespans;
-    std::vector<std::tuple<int, double,int>> rst;
+    std::vector<std::tuple<int, double,int,int>> rst;
 
     ioutils::TSVParser ss(FLAGS_stream);
     int t=0,ocalls=0;
@@ -46,9 +49,11 @@ int main(int argc, char* argv[]){
         hist_red.feed(a, segs);
         double val=hist_red.getResult();
         ocalls+=hist_red.statOracleCalls();
+        int num_algs=hist_red.size();
+        rst.emplace_back(t,val,ocalls,num_algs);
+
         hist_red.reduce();
         hist_red.next();
-        rst.emplace_back(t,val,ocalls);
         if(t==FLAGS_T) break;
     }
     std::string ofnm = osutils::join(
@@ -56,7 +61,7 @@ int main(int argc, char* argv[]){
             "histred_n{}b{}sieve_eps{}red_eps{}lmd{:g}L{}T{}.dat"_format(FLAGS_n,
                     FLAGS_B,FLAGS_sieve_eps,FLAGS_red_eps,FLAGS_lmd,
                     strutils::prettyNumber(FLAGS_L),strutils::prettyNumber(FLAGS_T)));
-    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
+    ioutils::saveTupleVec(rst, ofnm, "{}\t{}\t{}\t{}\n");
     printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
     return 0;
