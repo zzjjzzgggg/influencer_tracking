e72be443129ac11e64077b696485fc18a3276f13
J. Zhao <zzjjzzgggg@gmail.com>
2022-04-15 15:03:18 +0800

    exam_histit_red.cpp -> exam_hist.cpp: rename for better gflag support

--- src/exam_greedy.cpp
+++ src/exam_greedy.cpp
@@ -5,18 +5,18 @@
 #include "obj/graph_obj_fun.h"
 //#include "obj/checkin_obj_fun.h"
 #include "greedy_alg.h"
-#include <gflags/gflags.h>
 #include "eval_stream.h"
 
-DEFINE_string(dir, "../../result/greedy", "working directory");
-DEFINE_string(stream, "reddit.txt", "input streaming data file name");
-DEFINE_string(lifespans, "../../lifespans/lmd{:g}n{}L{}.gz",
-              "lifespans template");
+#include <gflags/gflags.h>
+
+DEFINE_string(dir, "", "working directory");
+DEFINE_string(stream, "stream.gz", "input stream");
+DEFINE_string(lifespans, "../lifespans/lmd{:g}n{}L{}.gz", "lifespans template");
 DEFINE_int32(n, 50, "number of samples");
-DEFINE_int32(B, 20, "budget");
-DEFINE_double(lmd, 0.002, "decaying rate");
-DEFINE_int32(L, 100000, "maximum lifetime");
-DEFINE_int32(T, 100, "end time");
+DEFINE_int32(B, 10, "budget");
+DEFINE_double(lmd, 0.001, "decaying rate");
+DEFINE_int32(L, 10000, "maximum lifetime");
+DEFINE_int32(T, 1000, "end time");
 
 int main(int argc, char* argv[]) {
     gflags::SetUsageMessage("usage:");
@@ -26,29 +26,33 @@ int main(int argc, char* argv[]) {
     /*** next line code is used for check in data ***/
     //    EvalStream<CheckinObjFun> eval(FLAGS_L);
 
-    EvalStream eval(FLAGS_L);
+    EvalStream eval(FLAGS_L, FLAGS_n);
 
     std::string lifespan_fnm =
         osutils::join(FLAGS_dir, fmt::format(FLAGS_lifespans, FLAGS_lmd, FLAGS_n,
                                              strutils::prettyNumber(FLAGS_L)));
     auto pin = ioutils::getIOIn(lifespan_fnm);
+    if (pin == nullptr) {
+        printf("File %s does not exist!\n", lifespan_fnm.c_str());
+        std::exit(-1);
+    }
+
     std::vector<int> lifespans;
-    ioutils::TSVParser ss(FLAGS_stream);
+    ioutils::TSVParser ss(osutils::join(FLAGS_dir, FLAGS_stream));
 
     std::vector<std::tuple<int, double, int>> rst;
     // std::vector<std::tuple<int, int>> user_num;
     int t = 0, ocalls = 0;
     /*** notes: we need change some code for different data ***/
-    while (ss.next() && (t++ < FLAGS_T)) {
-        int u = ss.get<int>(1), v = ss.get<int>(2);
-        Action a{u, v};
+    while (ss.next() && t++ < FLAGS_T) {
+        Action a{ss.get<int>(0), ss.get<int>(1)};
 
         lifespans.clear();
         pin->load(lifespans);
         ISetSegments segs(lifespans);
 
         eval.add(a, segs);
-        auto obj = eval.getObjMgr<GraphObjFun>(FLAGS_n);
+        auto obj = eval.getObjMgr<GraphObjFun>();
         GreedyAlg<GraphObjFun> greedy(&obj, FLAGS_B);
 
         auto users = eval.getNodes();
@@ -58,16 +62,21 @@ int main(int argc, char* argv[]) {
 
         double val = greedy.run(users);
         ocalls += greedy.getOracleCalls();
-        eval.next();
-
         rst.emplace_back(t, val, ocalls);
+
+        printf("\t%-12d%-12.2f%-12d%-12lu\r", t, val, ocalls, users.size());
+        fflush(stdout);
+
+        eval.next();
     }
+    printf("\n");
+
     std::string ofnm = osutils::join(
         FLAGS_dir,
         "greedy_lmd{:g}n{}k{}L{}T{}.dat"_format(FLAGS_lmd, FLAGS_n, FLAGS_B,
                                                 strutils::prettyNumber(FLAGS_L),
                                                 strutils::prettyNumber(FLAGS_T)));
-    ioutils::saveTripletVec(rst, ofnm, "{}\t{}\t{}\n");
+    ioutils::saveTripletVec(rst, ofnm, "{}\t{:.2f}\t{}\n");
 
     printf("cost time %s\n", tm.getStr().c_str());
     gflags::ShutDownCommandLineFlags();
---
+++ src/exam_hist.cpp
@@ -0,0 +1,73 @@
+//
+// Created by zhangwei on 2021/6/6.
+//
+
+#include "histit_red.h"
+//#include "obj/checkin_obj_fun.h"
+#include "obj/graph_obj_fun.h"
+
+#include <gflags/gflags.h>
+
+DEFINE_string(dir, "", "working directory");
+DEFINE_string(stream, "stream.gz", "input stream");
+DEFINE_string(lifespans, "../lifespans/lmd{:g}n{}L{}.gz", "lifespans template");
+DEFINE_int32(n, 50, "number of samples");
+DEFINE_int32(B, 10, "budget");
+DEFINE_double(eps, 0.1, "sievepait epsilon");
+DEFINE_double(lmd, 0.001, "decaying rate");
+DEFINE_int32(L, 10000, "maximum lifetime");
+DEFINE_int32(T, 1000, "end time");
+
+int main(int argc, char* argv[]) {
+    gflags::SetUsageMessage("usage:");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    osutils::Timer tm;
+
+    HistITRED<GraphObjFun> hist_red(FLAGS_n, FLAGS_B, FLAGS_eps, FLAGS_eps);
+    std::string lifespan_fnm =
+        osutils::join(FLAGS_dir, fmt::format(FLAGS_lifespans, FLAGS_lmd, FLAGS_n,
+                                             strutils::prettyNumber(FLAGS_L)));
+
+    auto pin = ioutils::getIOIn(lifespan_fnm);
+    if (pin == nullptr) {
+        printf("File %s does not exist!\n", lifespan_fnm.c_str());
+        std::exit(-1);
+    }
+
+    int t = 0, ocalls = 0;
+    std::vector<int> lifespans;
+    std::vector<std::tuple<int, double, int, int>> rst;
+
+    ioutils::TSVParser ss(osutils::join(FLAGS_dir, FLAGS_stream));
+    while (ss.next() && t++ < FLAGS_T) {
+        Action a{ss.get<int>(0), ss.get<int>(1)};
+
+        lifespans.clear();
+        pin->load(lifespans);
+        ISetSegments segs(lifespans);
+
+        hist_red.feed(a, segs);
+        double val = hist_red.getResult();
+        ocalls += hist_red.statOracleCalls();
+        int num_algs = hist_red.size();
+        rst.emplace_back(t, val, ocalls, num_algs);
+
+        printf("\t%-12d%-12.2f%-12d%-12d\r", t, val, ocalls, num_algs);
+        fflush(stdout);
+
+        hist_red.reduce();
+        hist_red.next();
+    }
+    printf("\n");
+
+    std::string ofnm =
+        osutils::join(FLAGS_dir, "hist_n{}k{}eps{:g}lmd{:g}L{}T{}.dat"_format(
+                                     FLAGS_n, FLAGS_B, FLAGS_eps, FLAGS_lmd,
+                                     strutils::prettyNumber(FLAGS_L),
+                                     strutils::prettyNumber(FLAGS_T)));
+    ioutils::saveTupleVec(rst, ofnm, "{}\t{:.2f}\t{}\t{}\n");
+
+    printf("cost time %s\n", tm.getStr().c_str());
+    gflags::ShutDownCommandLineFlags();
+    return 0;
+}
